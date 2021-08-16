d6e2146a84d8601a9772b7820ac9673bb3427f54
4bd4ll4h <4bd4ll4h.m@gmail.com>
2021-08-16 17:11:00 +0200

    v1.0

--- main.py
+++ main.py
@@ -31,13 +31,14 @@ async def handle(request ):
     if request.match_info.get('token') == bot.token:
         request_body_dict = await request.json()
         update = telebot.types.Update.de_json(request_body_dict)
+        
         bot.process_new_updates([update])
         return web.Response()
     else:
         return web.Response(status=403)
 
-app.router.add_post('/{token}/', handle)
-
+app.router.add_post(f"/{config['botToken']}/", handler= handle)
+app.router.add_get("/",handler=handle)
 # Main reply keyboard
 def mainReplyKeyboard(userLanguage):
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
